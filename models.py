10df15a47199870a02546cfa58a01a91397762f2
Nikhil Barhate <nikhilbarhate99@gmail.com>
2018-07-20 19:57:16 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,69 +1,58 @@
 import torch
 import torch.nn as nn
 
-# DCGAN Weight init :
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        m.weight.data.normal_(0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        m.weight.data.normal_(1.0, 0.02)
-        m.bias.data.fill_(0)
-
+class ResidualBlock(nn.Module):
+    def __init__(self, in_features):
+        super(ResidualBlock, self).__init__()
         
-class Generator(nn.Module):
+        self.conv_block = nn.Sequential(
+                
+                nn.Conv2d(in_features, in_features, 3, 1, 1),
+                nn.BatchNorm2d(in_features),
+                nn.ReLU(inplace=True),
+                
+                nn.Conv2d(in_features, in_features, 3, 1, 1),
+                nn.BatchNorm2d(in_features),
+                nn.ReLU(inplace=True) )
     
-    def __init__(self, nz=100):
+    def forward(self, x):
+        return self.conv_block(x) + x
+
+
+class Generator(nn.Module):
+    def __init__(self, in_ch, out_ch, n_blocks=4):
         super(Generator, self).__init__()
         
-        self.main = nn.Sequential(
-                
-            # input z, size: nz x 1 x 1
-            nn.ConvTranspose2d( nz, 512, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(512),
-            nn.ReLU(True),
-            
-            # state size: 512 x 4 x 4
-            nn.ConvTranspose2d( 512, 512, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.ReLU(True),
-            
-            # state size: 512 x 8 x 8
-            nn.ConvTranspose2d( 512, 512, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.ReLU(True),
-            
-            # state size: 512 x 32 x 32
-            nn.ConvTranspose2d( 512, 256, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.ReLU(True),
-            
-            # state size: 256 x 64 x 64
-            nn.ConvTranspose2d( 256, 128, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(128),
-            nn.ReLU(True),
-            
-            # state size: 128 x 128 x 128
-            nn.ConvTranspose2d( 128, 64, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(True),
-            
-            # state size: 64 x 256 x 256
-            nn.ConvTranspose2d( 64, 3, 4, 2, 1, bias=False),
-            nn.Tanh()
-            
-            # output size: 3 x 256 x 256
-            )
+        self.in_ch = in_ch
+        self.out_ch = out_ch
+        self.n_blocks = n_blocks
+        
+        # input size: 3 x 256 x 256
+        
+        n_features = 64
+        
+        # 1st conv:
+        model = [ nn.Conv2d(in_ch, n_features, 3, 1, 1),
+                  nn.BatchNorm2d(n_features),
+                  nn.ReLU(inplace=True) ]
+        
+        for i in range(self.n_blocks):
+            model += [ResidualBlock(n_features)]
+        
+        # last conv with tanh
+        model += [ nn.Conv2d(n_features, out_ch, 3, 1, 1),
+                   nn.Tanh() ]
+        
+        # output size: 3 x 256 x 256
+        
+        self.model = nn.Sequential(*model)
     
     def forward(self, input):
-        out = self.main(input)
+        return self.model(input)
         
-        return out
-
 
 class Discriminator(nn.Module):
-    
-    def __init__(self):
+    def __init__(self, in_ch):
         super(Discriminator, self).__init__()
         
         self.main = nn.Sequential(
@@ -72,12 +61,12 @@ class Discriminator(nn.Module):
             nn.Conv2d(3, 64, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
             
-            # state size: 64 x 128 x 128 
+            # state size: 64 x 128 x 128
             nn.Conv2d(64, 128, 4, 2, 1, bias=False),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
             
-            # state size: 128 x 64 x 64 
+            # state size: 128 x 64 x 64
             nn.Conv2d(128, 256, 4, 2, 1, bias=False),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, inplace=True),
@@ -88,47 +77,38 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(0.2, inplace=True),
             
             # state size: 512 x 16 x 16
-            nn.Conv2d(512, 512, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(512, 1, 4, 2, 1, bias=False),
             
-            # state size: 512 x 8 x 8
-            nn.MaxPool2d(kernel_size=4, stride=2, padding=1),
+            # state size: 1 x 8 x 8
+            nn.AvgPool2d(4, 2, 1),
             
-            # state size: 512 x 4 x 4
-            nn.Conv2d(512, 1, 4, 1, 0, bias=False),
-            nn.Sigmoid()
+            # state size: 1 x 4 x 4
+            nn.AvgPool2d(4, 1, 0),
             
-            # output size: 1 x 1 x 1
+            # state size: 1 x 1 x 1
+            nn.Sigmoid(),
             )
-    
+        
     def forward(self, input):
-        out = self.main(input)
-        
-        # out size: batch_size x 1 x 1 x 1
-        out = out.view(-1, 1).squeeze(1)
-        
-        # out size: 1 x batch_size
-        return out 
+        return self.main(input)
+
 
 
 
 ## TESTING CODE :
 #device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 #
-#dis = Discriminator().to(device)
+#dis = Discriminator(3).to(device)
 #
-#gen = Generator().to(device)
+#gen = Generator(3,3).to(device)
 #
-#z = torch.randn(1,100,1,1).to(device)
+#z = torch.randn(1,3,256,256).to(device)
 #
-#print(z.size())
+#print('Random image size {}'.format(z.size()))
 #
 #fake = gen(z)
 #
-#print(fake.size())
+#print('Fake image size {}'.format(fake.size()))
 #
 #print(dis(fake))
-
-    
-
+#
